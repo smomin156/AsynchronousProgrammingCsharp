3bb56ecfc0b8bb25927e8ccecf55d8b1ce147f70
smomin156 <63006524+smomin156@users.noreply.github.com>
2021-08-29 14:07:29 +0530

    Add files via upload

---
+++ AsysnchronousProgramming/AsysnchronousProgramming/AnotherExample.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace AsysnchronousProgramming
+{
+    class AnotherExample
+    {
+        private const string URL = "https://docs.microsoft.com/en-us/dotnet/csharp/csharp";
+
+        static void Main(string[] args)
+        {
+            DoSynchronousWork();
+            var someTask = DoSomethingAsync();
+            DoSynchronousWorkAfterAwait();
+            someTask.Wait(); //this is a blocking call, use it only on Main method
+            Console.WriteLine("Press nter");
+            Console.ReadLine();
+        }
+        public static void DoSynchronousWork()
+        {
+            // You can do whatever work is needed here
+            Console.WriteLine("1. Doing some work synchronously");
+        }
+
+        static async Task DoSomethingAsync() //A Task return type will eventually yield a void
+        {
+            Console.WriteLine("2. Async task has started...");
+            await GetStringAsync(); // we are awaiting the Async Method GetStringAsync
+        }
+
+        static async Task GetStringAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                Console.WriteLine("3. Awaiting the result of GetStringAsync of Http Client...");
+                string result = await httpClient.GetStringAsync(URL); 
+                Console.WriteLine("4. The awaited task has completed. Let's get the content length...");
+                Console.WriteLine($"5. The length of http Get for {URL}");
+                Console.WriteLine($"6. {result.Length} character");
+            }
+        }
+        static void DoSynchronousWorkAfterAwait()
+        {
+            //This is the work we can do while waiting for the awaited Async Task to complete
+            Console.WriteLine("7. While waiting for the async task to finish, we can do some unrelated work...");
+            for (var i = 0; i <= 5; i++)
+            {
+                for (var j = i; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ AsysnchronousProgramming/AsysnchronousProgramming/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsysnchronousProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello\n");
+            DoSomethingAsync();
+            DoSomething2();
+
+            Console.ReadLine();
+        }
+
+        public static async Task DoSomethingAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"Method 1 ={i}");
+                }
+            });
+        }
+
+        public static void DoSomething2()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"Method 2 =={i}");
+            }
+        }
+    }
+}
